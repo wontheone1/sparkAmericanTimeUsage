c96d3a19a03c666161e721d148d6a5c2265ece4d
WON SEOB SEO <won.seob.seo@zalando.fi>
2017-09-10 22:46:19 +0300

    Implement classified columns

--- src/main/scala/timeusage/TimeUsage.scala
+++ src/main/scala/timeusage/TimeUsage.scala
@@ -87,7 +87,25 @@ object TimeUsage {
     *    “t10”, “t12”, “t13”, “t14”, “t15”, “t16” and “t18” (those which are not part of the previous groups only).
     */
   def classifiedColumns(columnNames: List[String]): (List[Column], List[Column], List[Column]) = {
-    ???
+    def findGroup(name: String): Option[Int] = {
+      val categoryMappings = List(
+        0 -> List("t01", "t03", "t11", "t1801", "t1803"),
+        1 -> List("t05", "t1805"),
+        2 -> List("t02", "t04", "t06", "t07", "t08", "t09", "t10", "t12", "t13", "t14", "t15", "t16", "t18")
+      )
+
+      categoryMappings.flatMap {
+        case (index, prefixes) => if (prefixes.exists(name.startsWith)) Some(index) else None
+      }.sorted.headOption
+    }
+
+    val groups = columnNames.groupBy(findGroup).collect{
+      case(Some(index), names) => index -> names.map(name => new Column(name))
+    }
+
+    val results =  (0 to 2).map(index => groups.getOrElse(index, List.empty[Column]))
+
+    (results(0), results(1), results(2))
   }
 
   /** @return a projection of the initial DataFrame such that all columns containing hours spent on primary needs
--- src/test/scala/timeusage/TimeUsageSuite.scala
+++ src/test/scala/timeusage/TimeUsageSuite.scala
@@ -14,6 +14,9 @@ class TimeUsageSuite extends FunSuite with BeforeAndAfterAll {
 
   lazy val timeUsage = TimeUsage
 
+  lazy val (columns, initDf) = timeUsage.read("/timeusage/testTimeUsage.csv")
+  lazy val (primaryNeedsColumns, workColumns, otherColumns) = timeUsage.classifiedColumns(columns)
+
   test("timeUsage") {
     assert(timeUsage.spark.sparkContext.appName === "Time Usage")
     assert(timeUsage.spark.sparkContext.isStopped === false)
@@ -35,5 +38,21 @@ class TimeUsageSuite extends FunSuite with BeforeAndAfterAll {
     assert(testRow(0).getClass.getName === "java.lang.String")
     assert(testRow(1).getClass.getName === "java.lang.Double")
     assert(testRow(2).getClass.getName === "java.lang.Double")
+  }
+
+  test("classifiedColumns") {
+    val primaryNeeds = primaryNeedsColumns.map(_.toString)
+    val works = workColumns.map(_.toString)
+    val others = otherColumns.map(_.toString)
+
+    assert(primaryNeeds.contains("t010199"))
+    assert(primaryNeeds.contains("t030501"))
+    assert(primaryNeeds.contains("t110101"))
+    assert(primaryNeeds.contains("t180382"))
+    assert(works.contains("t050103"))
+    assert(works.contains("t180589"))
+    assert(others.contains("t020101"))
+    assert(others.contains("t180699"))
+
   }
 }
